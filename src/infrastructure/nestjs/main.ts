69b427e45d4cbe407951ff089443dadf04d5dda9
Gabriel Oliveira Lima <gabriel.lima@artesanalinvestimentos.com.br>
2025-11-11 16:02:56 -0300

    feat(api): add swagger docs and global validation pipe
    Add Swagger documentation setup and global validation pipe for API requests. Also includes global exception filter for consistent error handling.

--- src/infrastructure/nestjs/main.ts
+++ src/infrastructure/nestjs/main.ts
@@ -1,8 +1,23 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import 'dotenv/config';
+import { HttpExceptionFilter } from './filters/http-exception.filter';
 import { AppModule } from './modules/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Financial API Control')
+    .setVersion('1.0')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, documentFactory);
+
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
