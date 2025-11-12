0c1e5615495c224d10fdd337098ec74e0873d577
Gabriel Oliveira Lima <gabriel.lima@artesanalinvestimentos.com.br>
2025-11-12 18:04:20 -0300

    feat(account-balance): add account balance port interface
    Add interface for account balance operations with create method

---
+++ src/core/port/account-balance.port.ts
@@ -0,0 +1,5 @@
+import { AccountBalanceDomain } from "../domain/account-balance/account-balance.domain";
+
+export interface AccountBalancePort {
+  createAccountBalance(amout: number, userId: string): Promise<AccountBalanceDomain>;
+}
