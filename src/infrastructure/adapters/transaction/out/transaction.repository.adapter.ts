f904e4efafa3edaa7da615901374d18b800d45e4
Gabriel Oliveira Lima <gabriel.lima@artesanalinvestimentos.com.br>
2025-11-12 18:04:08 -0300

    feat(transaction): add method to find transactions by user id

--- src/infrastructure/adapters/transaction/out/transaction.repository.adapter.ts
+++ src/infrastructure/adapters/transaction/out/transaction.repository.adapter.ts
@@ -3,12 +3,43 @@ import { CategoryTypeEnumValues } from "../../../../core/domain/categories/dto";
 import { PaymentMethodValues, TransactionStatusValues } from "../../../../core/domain/transactions/dto";
 import { TransactionsDomain } from "../../../../core/domain/transactions/transactions.domain";
 import { TransactionPort } from "../../../../core/port/transaction.port";
+import { CategoryDomainAdapter } from "../../category/in/category.domain.adapter";
 import { TransactionDomainAdapter } from "../in/transaction.domain.adapter";
 
 export class PrismaTransactionRepository implements TransactionPort {
   constructor(
     private readonly prisma: PrismaClient
   ) {}
+
+  async findTransactionsByUser(userId: string): Promise<TransactionsDomain[]> {
+    const transactions = await this.prisma.transaction.findMany({
+      where: {
+        userId,
+      },
+      include: {
+        category: true,
+      },
+    });
+
+    return transactions.map((transaction) => {
+      const paymentMethod = PaymentMethodValues.find((value) => value === transaction.paymentMethod);
+      const transactionStatus = TransactionStatusValues.find((value) => value === transaction.transactionStatus);
+      const transactionType = CategoryTypeEnumValues.find((value) => value === transaction.transactionType);
+      const categoryType = CategoryTypeEnumValues.find((value) => value === transaction.category?.categoryType);
+
+      return TransactionDomainAdapter.toDomain({
+        amount: transaction.amount.toNumber(),
+        paymentMethod: paymentMethod!,
+        transactionDate: transaction.transactionDate,
+        transactionStatus: transactionStatus,
+        transactionType: transactionType!,
+        category: transaction.category ? CategoryDomainAdapter.toDomain({
+          categoryName: transaction.category.categoryName,
+          categoryType: categoryType!,
+        }) : undefined,
+      })
+    });
+  }
 
   async createTransaction(transaction: TransactionsDomain, userId: string): Promise<TransactionsDomain> {
     const createdTransaction = await this.prisma.transaction.create({
