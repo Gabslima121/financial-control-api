5d7f6005312e2375fba0d1d53a7090d7353c939c
Gabriel Oliveira Lima <gabriel.lima@artesanalinvestimentos.com.br>
2025-11-12 18:04:38 -0300

    feat(account-balance): add repository adapter for account balance
    Implement AccountBalanceRepositoryAdapter to handle account balance creation
    using Prisma client and convert to domain model

---
+++ src/infrastructure/adapters/account-balance/out/account-balance.repository.adapter.ts
@@ -0,0 +1,27 @@
+import { PrismaClient } from "@prisma/client";
+import { AccountBalanceDomain } from "../../../../core/domain/account-balance/account-balance.domain";
+import { AccountBalancePort } from "../../../../core/port/account-balance.port";
+import { AccountBalanceDomainAdapter } from "../in/account-balance.domain.adapter";
+
+export class AccountBalanceRepositoryAdapter implements AccountBalancePort {
+  constructor(
+    private readonly prisma: PrismaClient,
+  ) {}
+
+  async createAccountBalance(amount: number, userId: string): Promise<AccountBalanceDomain> {
+    const accountBalance = await this.prisma.accountBalance.create({
+      data: {
+        balance: amount,
+        userId,
+        balanceDate: new Date(),
+      }
+    })
+
+    return AccountBalanceDomainAdapter.toDomain({
+      balanceId: accountBalance.balanceId,
+      balance: accountBalance.balance.toNumber(),
+      balanceDate: accountBalance.balanceDate,
+      description: accountBalance.notes || '',
+    });
+  }
+}
