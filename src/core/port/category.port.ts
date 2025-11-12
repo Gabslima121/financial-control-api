02b640b54bc83c3f8b04738af4506f830f689e9f
Gabriel Oliveira Lima <gabriel.lima@artesanalinvestimentos.com.br>
2025-11-12 16:07:51 -0300

    feat(category): add findCategoryById method to port and repository
    Implement method to find category by ID in the category port and repository

--- src/core/port/category.port.ts
+++ src/core/port/category.port.ts
@@ -4,4 +4,5 @@ import { CategoryTypeEnum } from '../domain/categories/dto';
 export interface CategoryPort {
   createCategory(category: CategoriesDomain, userId: string): Promise<CategoriesDomain>;
   listCategoryByType(type: CategoryTypeEnum): Promise<CategoriesDomain[]>;
+  findCategoryById(categoryId: string): Promise<CategoriesDomain | null>;
 }
--- src/infrastructure/adapters/category/out/category.repository.adapter.ts
+++ src/infrastructure/adapters/category/out/category.repository.adapter.ts
@@ -1,11 +1,37 @@
 import { PrismaClient } from "@prisma/client";
 import { CategoriesDomain } from "../../../../core/domain/categories/categories.domain";
-import { CategoryTypeEnum } from "../../../../core/domain/categories/dto";
+import { CategoryTypeEnum, CategoryTypeEnumValues } from "../../../../core/domain/categories/dto";
 import { CategoryPort } from "../../../../core/port/category.port";
 import { CategoryDomainAdapter } from "../in/category.domain.adapter";
 
 export class PrismaCategoryRepository implements CategoryPort {
   constructor(private readonly prisma: PrismaClient) {}
+
+  async findCategoryById(categoryId: string): Promise<CategoriesDomain | null> {
+    const category = await this.prisma.category.findFirst({
+      where: {
+        categoryId
+      }
+    });
+
+    if (!category) {
+      return null;
+    }
+
+    const categoryType = CategoryTypeEnumValues.find((item) => item === category.categoryType);
+
+    if (!categoryType) {
+      return null;
+    }
+
+    return CategoryDomainAdapter.toDomain({
+      categoryId: category.categoryId,
+      categoryName: category.categoryName,
+      categoryType,
+      createdAt: category.createdAt,
+      description: category.description,
+    })
+  }
 
   async createCategory(category: CategoriesDomain, userId: string): Promise<CategoriesDomain> {
     const entity = await this.prisma.category.create({
